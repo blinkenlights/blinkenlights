bbb87d6dcfb87b2ee83f5cd09d0249691b328a3b
daniel <daniel@fe0de55c-ccef-0310-be5c-e8635c8c2365>
2008-08-19 03:04:56 +0000

    new commands
    git-svn-id: file:///home/daniel/src/blinkenlights/legacy/svn/blinkenlights/trunk@208 fe0de55c-ccef-0310-be5c-e8635c8c2365

--- code/mcu/wmcu_config/wmcu_configurator.py
+++ code/mcu/wmcu_config/wmcu_configurator.py
@@ -7,28 +7,37 @@ import struct
 
 def usage():
 	print("Blinkenlights Wireless MCU setup tool\n")
-	print("Usage: %s [--help] [--host <ip>] [--port <port>] [--set-line value] [--set-lamp-id value]" % sys.argv[0])
-	print("\t--help			this screen")
-	print("\t--host <ip>		the IP address to connect to")
-	print("\t--port <port>		the port to use, defaults to 2323")
-	print("\t--set-line <line>	configure the WMCU to listen to line #<line>")
-	print("\t--set-lamp-id <id>	sets the id of an lamp, requires --lamp-mac")
-	print("\t--lamp-mac <id>	specify the lamp MAC address to use for other commands")
+	print("Usage: %s [--help] [options]" % sys.argv[0])
+	print("\t--help				this screen")
+	print("\t--host <ip>			the IP address to connect to")
+	print("\t--port <port>			the port to use, defaults to 2323")
+	print("\t--set-line <line>		configure the WMCU to listen to line #<line>")
+	print("\t--set-lamp-id <id>		sets the id of an lamp, requires --lamp-mac")
+	print("\t--set-gamma <g1>,<g2>,...<g8>	sets the gamma curve for a lamp");
+	print("\t--write-gamma			makes the lamp write its gamma curve");
+	print("\t--lamp-mac <id>		specify the lamp MAC address to use for other commands (0xfff for broadcast)")
 	sys.exit(1)
 
 action = -1
 line = -1
 lampmac = 0
 
-host = ""
-port = 2323
+params		= [0, 0, 0, 0, 0, 0, 0, 0];
 
-SET_LINE = 0
-SET_LAMPID = 1
-MCUCTRL_MAGIC = 0x23542667
+host 		= ""
+port		= 2323
+
+SET_LINE 	= 0
+SET_LAMPID 	= 1
+SET_GAMMA 	= 2
+WRITE_GAMMA 	= 3
+MCUCTRL_MAGIC 	= 0x23542667
 
 try:
-	opts, args = getopt.getopt(sys.argv[1:], "hh:p:s:s:l:", ["help", "host=", "port=", "set-line=", "set-lamp-id=", "lamp-mac="])
+	opts, args = getopt.getopt(sys.argv[1:],
+		"hh:p:s:s:s:wl:", 
+		["help", "host=", "port=", "set-line=", "set-lamp-id=", "set-gamma=", "write-gamma", "lamp-mac="])
+
 except getopt.GetoptError, err:
 	print str(err)
 	usage()
@@ -41,25 +50,44 @@ for o, a in opts:
 	if o == "--port":
 		port = int(a)
 	if o == "--lamp-mac":
-		lampid = int(a)
+		lampmac = int(a, 16)
 	if o == "--set-line":
 		action = SET_LINE
 		line = int(a)
 	if o == "--set-lamp-id":
 		action = SET_LAMPID
 		lampid = int(a)
+	if o == "--set-gamma":
+		action = SET_GAMMA
+		# XXX
+	if o == "--write-gamma":
+		action = WRITE_GAMMA
 
 if action == -1:
 	print("need an action to perform.\n")
 	usage()
 
 if action == SET_LINE:
-	packet = struct.pack("!IIII", MCUCTRL_MAGIC, 0, line, 0)
+	packet = struct.pack("!IIII8I", MCUCTRL_MAGIC, 0, line, 0,
+			params[0], params[1], params[2], params[3],
+			params[4], params[5], params[6], params[7])
+
 elif action == SET_LAMPID:
 	if lampmac == 0:
 		usage()
 
-	packet = struct.pack("!IIII", MCUCTRL_MAGIC, 1, lampid, lampmac)
+	packet = struct.pack("!IIII8I", MCUCTRL_MAGIC, 1, lampmac, lampid, 
+			params[0], params[1], params[2], params[3],
+			params[4], params[5], params[6], params[7])
+
+elif action == SET_GAMMA:
+	if lampmac == 0:
+		usage()
+	
+	packet = struct.pack("!IIII8I", MCUCTRL_MAGIC, 2, lampmac, 0,
+			params[0], params[1], params[2], params[3],
+			params[4], params[5], params[6], params[7])
+	
 
 if host == "":
 	usage()
