b60afd5c92f7ec4734f45fa4674513a45865f06f
tim <tim@fe0de55c-ccef-0310-be5c-e8635c8c2365>
2008-08-21 09:45:14 +0000

    Comments
    git-svn-id: file:///home/daniel/src/blinkenlights/legacy/svn/blinkenlights/trunk@264 fe0de55c-ccef-0310-be5c-e8635c8c2365

--- setup/stereoscope/barcode/create-barcodes.sh
+++ setup/stereoscope/barcode/create-barcodes.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+#
+# Create 3x6 (18) Code128b barcodes per page on DIN A4 size
+#
+# This tool requires GNU Barcode to be installed
 
 barcode -e "128b" -u mm -p 210x297mm -t 3x6+10+5-5-23 -m 5 -o barcode.ps -i wdim-macs.txt
