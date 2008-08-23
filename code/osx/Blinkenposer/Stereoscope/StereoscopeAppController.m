1a6a84f609ffb5d9b2bfaa9348b7feca84438fbc
dom <dom@fe0de55c-ccef-0310-be5c-e8635c8c2365>
2008-08-23 18:14:38 +0000

    - added saving of arrangement, use proxy and proxy address and port
    git-svn-id: file:///home/daniel/src/blinkenlights/legacy/svn/blinkenlights/trunk@283 fe0de55c-ccef-0310-be5c-e8635c8c2365

--- code/osx/Blinkenposer/Stereoscope/StereoscopeAppController.m
+++ code/osx/Blinkenposer/Stereoscope/StereoscopeAppController.m
@@ -2,6 +2,23 @@
 
 @implementation StereoscopeAppController
 
+- (NSArray *)keysToSaveAndRestore
+{
+	return [NSArray arrayWithObjects:@"Proxy_Address",@"Proxy_Port",@"Use_Proxy", @"Arrangement",nil];
+}
+
+- (void)applicationWillTerminate:(NSNotification *)inNotification
+{
+	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	
+	for (NSString *key in [self keysToSaveAndRestore]) {
+		id object = [_ibQCView valueForInputKey:key];
+		if (object) {
+			[defaults setObject:object forKey:key];
+		}
+	}
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)inNotification 
 {
 //	NSString *compositionPath = [[NSBundle mainBundle] pathForResource:@"Stereoscope" ofType:@"qtz"];
@@ -10,6 +27,15 @@
 
 	[_ibQCView setEventForwardingMask:NSAnyEventMask];
 	[_ibQCView startRendering];
+	
+	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	
+	for (NSString *key in [self keysToSaveAndRestore]) {
+		id object = [defaults objectForKey:key];
+		if (object) {
+			[_ibQCView setValue:object forInputKey:key];
+		}
+	}
 }
 
 - (IBAction)changeViewPosition:(id)sender {
