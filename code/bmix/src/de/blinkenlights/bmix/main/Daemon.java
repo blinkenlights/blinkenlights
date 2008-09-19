a7282d6c98a571696636ef786955409626b55f7d
andrew <andrew@fe0de55c-ccef-0310-be5c-e8635c8c2365>
2008-09-19 22:06:32 +0000

    removed overrides in the Daemon class so that it compiles in Java 1.5
    git-svn-id: file:///home/daniel/src/blinkenlights/legacy/svn/blinkenlights/trunk@405 fe0de55c-ccef-0310-be5c-e8635c8c2365

--- code/bmix/src/de/blinkenlights/bmix/main/Daemon.java
+++ code/bmix/src/de/blinkenlights/bmix/main/Daemon.java
@@ -3,27 +3,22 @@ package de.blinkenlights.bmix.main;
 import org.apache.commons.daemon.DaemonContext;
 
 public class Daemon implements org.apache.commons.daemon.Daemon {
-
 	private String[] arguments;
 
-	@Override
 	public void destroy() {
 		// no special cleanup required
 	}
 
-	@Override
 	public void init(DaemonContext arg0) throws Exception {
 		arguments = arg0.getArguments();
 		
 	}
 
-	@Override
 	public void start() throws Exception {
 		// BMix.main starts a thread and returns...
 		BMix.main(arguments);
 	}
 
-	@Override
 	public void stop() throws Exception {
 		// no special cleanup....		
 		// TODO: possibly blank the screen?
