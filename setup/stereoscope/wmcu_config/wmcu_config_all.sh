996d9331731151b8242050485523c448b3b4372c
daniel <daniel@fe0de55c-ccef-0310-be5c-e8635c8c2365>
2008-09-24 19:34:56 +0000

    added
    git-svn-id: file:///home/daniel/src/blinkenlights/legacy/svn/blinkenlights/trunk@476 fe0de55c-ccef-0310-be5c-e8635c8c2365

---
+++ setup/stereoscope/wmcu_config/wmcu_config_all.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+./wmcu_configurator.py --host 192.168.1.4 --set-mcu-id 4
+./wmcu_configurator.py --host 192.168.1.5 --set-mcu-id 5
+./wmcu_configurator.py --host 192.168.1.6 --set-mcu-id 6
+./wmcu_configurator.py --host 192.168.1.7 --set-mcu-id 7
+./wmcu_configurator.py --host 192.168.1.8 --set-mcu-id 8
+./wmcu_configurator.py --host 192.168.1.9 --set-mcu-id 9
+./wmcu_configurator.py --host 192.168.1.10 --set-mcu-id 10
+
+./wmcu_configurator.py --host 192.168.2.12 --set-mcu-id 12
+./wmcu_configurator.py --host 192.168.2.13 --set-mcu-id 13
+./wmcu_configurator.py --host 192.168.2.14 --set-mcu-id 14
+./wmcu_configurator.py --host 192.168.2.15 --set-mcu-id 15
+./wmcu_configurator.py --host 192.168.2.16 --set-mcu-id 16
+./wmcu_configurator.py --host 192.168.2.17 --set-mcu-id 17
+./wmcu_configurator.py --host 192.168.2.18 --set-mcu-id 18
+./wmcu_configurator.py --host 192.168.2.19 --set-mcu-id 19
+
+./wmcu_configurator.py --host 192.168.3.4 --set-mcu-id 104
+./wmcu_configurator.py --host 192.168.3.5 --set-mcu-id 105
+./wmcu_configurator.py --host 192.168.3.6 --set-mcu-id 106
+./wmcu_configurator.py --host 192.168.3.7 --set-mcu-id 107
+./wmcu_configurator.py --host 192.168.3.8 --set-mcu-id 108
+./wmcu_configurator.py --host 192.168.3.9 --set-mcu-id 109
+./wmcu_configurator.py --host 192.168.3.10 --set-mcu-id 110
+./wmcu_configurator.py --host 192.168.3.11 --set-mcu-id 111
+./wmcu_configurator.py --host 192.168.3.12 --set-mcu-id 112
+
+./wmcu_configurator.py --host 192.168.4.14 --set-mcu-id 114
+./wmcu_configurator.py --host 192.168.4.15 --set-mcu-id 115
+./wmcu_configurator.py --host 192.168.4.16 --set-mcu-id 116
+./wmcu_configurator.py --host 192.168.4.17 --set-mcu-id 117
+./wmcu_configurator.py --host 192.168.4.18 --set-mcu-id 118
+./wmcu_configurator.py --host 192.168.4.19 --set-mcu-id 119
+./wmcu_configurator.py --host 192.168.4.20 --set-mcu-id 120
+./wmcu_configurator.py --host 192.168.4.21 --set-mcu-id 121
+./wmcu_configurator.py --host 192.168.4.22 --set-mcu-id 122
+./wmcu_configurator.py --host 192.168.4.23 --set-mcu-id 123
+./wmcu_configurator.py --host 192.168.4.24 --set-mcu-id 124
+./wmcu_configurator.py --host 192.168.4.25 --set-mcu-id 125
+
