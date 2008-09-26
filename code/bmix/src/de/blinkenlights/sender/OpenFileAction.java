5fa65c544473239e4dccd8ea28f7317a9600e6a2
jfuerth <jfuerth@fe0de55c-ccef-0310-be5c-e8635c8c2365>
2008-09-26 03:58:35 +0000

    New GUI sender thingy with drag and drop!
    Just needs a build target and we're good to go
    
    git-svn-id: file:///home/daniel/src/blinkenlights/legacy/svn/blinkenlights/trunk@501 fe0de55c-ccef-0310-be5c-e8635c8c2365

---
+++ code/bmix/src/de/blinkenlights/sender/OpenFileAction.java
@@ -0,0 +1,37 @@
+/*
+ * Created on Sep 25, 2008
+ */
+package de.blinkenlights.sender;
+
+import java.awt.FileDialog;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
+
+public class OpenFileAction extends AbstractAction {
+
+    private final Frame parentFrame;
+    private final Sender sender;
+
+    public OpenFileAction(Frame parentFrame, Sender sender) {
+        super("Open...");
+        this.parentFrame = parentFrame;
+        this.sender = sender;
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        FileDialog fd = new FileDialog(parentFrame, "Choose a BML File");
+        fd.setVisible(true);
+        String dir = fd.getDirectory();
+        String file = fd.getFile();
+        if (dir == null || file == null) {
+            JOptionPane.showMessageDialog(parentFrame, "Ok, maybe next time");
+            System.exit(0);
+        }
+        File chosenFile = new File(dir, file);
+        sender.sendMovie(chosenFile);
+    }
+}
---
+++ code/bmix/src/de/blinkenlights/sender/Sender.java
@@ -0,0 +1,141 @@
+/*
+ * Created on Sep 25, 2008
+ */
+package de.blinkenlights.sender;
+
+import java.awt.BorderLayout;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.net.InetAddress;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+import de.blinkenlights.bmix.main.BMovieException;
+import de.blinkenlights.bmix.main.BMovieSender;
+
+public class Sender {
+
+    private static final boolean MAC_OS_X = (System.getProperty("os.name").toLowerCase().startsWith("mac os x")); //$NON-NLS-1$ //$NON-NLS-2$
+    private final JLabel statusLabel;
+    private final JFrame frame;
+    private BMovieSender movieSender;
+    private JTextField sendToHost;
+    private File currentFile;
+
+    public Sender() {
+        frame = new JFrame("Blinkensender");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JMenuBar menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+        
+        JMenu m = new JMenu("File");
+        menuBar.add(m);
+        
+        m.add(new JMenuItem(new OpenFileAction(frame, this)));
+        
+        statusLabel = new JLabel("Drag BML file here!", JLabel.CENTER);
+        frame.add(statusLabel);
+
+        JPanel configPanel = new JPanel();
+        configPanel.add(new JLabel("Send to"));
+        configPanel.add(sendToHost = new JTextField("localhost"));
+        sendToHost.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                sendMovie(currentFile);
+            }
+        });
+        frame.add(configPanel, BorderLayout.SOUTH);
+        
+        frame.setSize(500, 200);
+        
+        frame.setDropTarget(new DropTarget(frame, new FileDropListener()));
+        frame.setVisible(true);
+
+    }
+    
+    public void sendMovie(File file) {
+        currentFile = file;
+        if (currentFile == null) {
+            return;
+        }
+        statusLabel.setText("Sending " + file.getAbsolutePath() + " to "+sendToHost.getText()+":2323");
+        try {
+            if (movieSender != null) {
+                movieSender.stopSending();
+            }
+            movieSender = new BMovieSender(file.getAbsolutePath(), sendToHost.getText(), 2323, true);
+            movieSender.start();
+        } catch (BMovieException e) {
+            JOptionPane.showMessageDialog(frame, "Couldn't send movie:\n" + e);
+        }
+    }
+
+    public class FileDropListener implements DropTargetListener {
+
+        public void dragEnter(DropTargetDragEvent dtde) {
+            dtde.acceptDrag(DnDConstants.ACTION_COPY);
+        }
+
+        public void dragExit(DropTargetEvent dte) {
+            // don't care
+        }
+
+        public void dragOver(DropTargetDragEvent dtde) {
+            dtde.acceptDrag(DnDConstants.ACTION_COPY);
+        }
+
+        public void drop(DropTargetDropEvent dtde) {
+            try {
+                System.out.println("Got drop event: " + dtde);
+                Transferable t = dtde.getTransferable();
+                List flavorList = dtde.getCurrentDataFlavorsAsList();
+                if (t.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                    dtde.acceptDrop(DnDConstants.ACTION_COPY);
+                    List fileList = (List) t.getTransferData(DataFlavor.javaFileListFlavor);
+                    if (fileList.size() > 0) {
+                        sendMovie((File) fileList.get(0));
+                    }
+                    System.out.println(fileList);
+                } else {
+                    dtde.rejectDrop();
+                    System.out.println("No supported flavours in drop event: " + flavorList);
+                }
+                dtde.dropComplete(true);
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Drop failed: " + ex.getMessage());
+            }
+        }
+
+        public void dropActionChanged(DropTargetDragEvent dtde) {
+            // don't care
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Sender();
+            }
+        });
+    }
+}
