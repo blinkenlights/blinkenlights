4ca88a94541ad73d75ce2d89579288bd53a0016b
dfraser <dfraser@fe0de55c-ccef-0310-be5c-e8635c8c2365>
2008-09-29 17:22:59 +0000

    now supports outputting the metadata header
    git-svn-id: file:///home/daniel/src/blinkenlights/legacy/svn/blinkenlights/trunk@689 fe0de55c-ccef-0310-be5c-e8635c8c2365

--- code/bmix/src/de/blinkenlights/bmix/movie/BMLOutputStream.java
+++ code/bmix/src/de/blinkenlights/bmix/movie/BMLOutputStream.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Map;
 
 /**
  * Outputs a BML (Blinkenlights Markup Language) from a sequence
@@ -19,6 +20,7 @@ public class BMLOutputStream extends OutputStream {
 	private final Dimension size;
 	private final int bpp;
 	private boolean closed = false;
+	private final Map<String, String> headerData;
 
 	/**
 	 * Opens a BML Output Stream and writes an appropriate header to the stream.
@@ -27,10 +29,13 @@ public class BMLOutputStream extends OutputStream {
 	 * @param fps the frames per second of the movie
 	 * @param size the size of each frame
 	 * @param bpp the bits-per-pixel of each frame in the movie.  8bpp and 4bpp are supported.
+	 * @param headerData key/value data for the Header element of the BML file.  
+	 * Each entry in the map will be turned into an XML element.
 	 * @throws IOException if the wrapped output stream does.
 	 */
-	public BMLOutputStream(OutputStream out, int fps, Dimension size, int bpp) throws IOException {
+	public BMLOutputStream(OutputStream out, int fps, Dimension size, int bpp, Map<String,String> headerData) throws IOException {
 		this.out = out;
+		this.headerData = headerData;
 		this.millisPerFrame = 1000 / fps;
 		this.size = size;
 		if (bpp != 4 && bpp != 8) {
@@ -45,6 +50,31 @@ public class BMLOutputStream extends OutputStream {
 		StringBuilder outStr = new StringBuilder();
 		outStr.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
 		outStr.append("<blm width=\""+size.width+"\" height=\""+size.height+"\" bits=\""+bpp+"\" channels=\"1\">\n");
+		outStr.append("<header>\n");
+		for (String headerKey : headerData.keySet()) {
+			String headerValue = headerData.get(headerKey);
+			
+			if (headerKey.equals("title") && headerValue != null && headerValue.length() > 0) {
+				outStr.append("<title>"+headerValue+"</title>\n");
+			}
+			else if (headerKey.equals("description") && headerValue != null && headerValue.length() > 0) {
+				outStr.append("<description>"+headerValue+"</description>\n");
+			}
+			else if (headerKey.equals("author") && headerValue != null && headerValue.length() > 0) {
+				outStr.append("<author>"+headerValue+"</author>\n");
+			}
+			else if (headerKey.equals("email") && headerValue != null && headerValue.length() > 0) {
+				outStr.append("<email>"+headerValue+"</email>\n");
+			}
+			else if (headerKey.equals("url") && headerValue != null && headerValue.length() > 0) {
+				outStr.append("<url>"+headerValue+"</url>\n");
+			} else {
+				System.err.println("unsupported header found in map: "+headerKey);
+			}
+		}
+		outStr.append("<creator>$Id$</creator>\n");
+		outStr.append("</header>\n");
+		
 		byte[] bytes = outStr.toString().getBytes();
 		out.write(bytes, 0, bytes.length);
 	}
