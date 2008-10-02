018d5ecce9992c02d0cb2f4ba3ad211bfab5af24
daniel <daniel@fe0de55c-ccef-0310-be5c-e8635c8c2365>
2008-10-02 23:32:17 +0000

    added
    git-svn-id: file:///home/daniel/src/blinkenlights/legacy/svn/blinkenlights/trunk@772 fe0de55c-ccef-0310-be5c-e8635c8c2365

---
+++ setup/stereoscope/wmcu_config/set_lampmap_all.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+./wmcu_configurator.py --host 192.168.104.25 --set-assigned-lamps ../lampmaps/proc/lampmap-e-25-0.proc
+./wmcu_configurator.py --host 192.168.104.24 --set-assigned-lamps ../lampmaps/proc/lampmap-e-24-1.proc
+./wmcu_configurator.py --host 192.168.104.23 --set-assigned-lamps ../lampmaps/proc/lampmap-e-23-2.proc
+./wmcu_configurator.py --host 192.168.104.22 --set-assigned-lamps ../lampmaps/proc/lampmap-e-22-3.proc
+./wmcu_configurator.py --host 192.168.104.21 --set-assigned-lamps ../lampmaps/proc/lampmap-e-21-4.proc
+./wmcu_configurator.py --host 192.168.104.20 --set-assigned-lamps ../lampmaps/proc/lampmap-e-20-5.proc
+./wmcu_configurator.py --host 192.168.104.19 --set-assigned-lamps ../lampmaps/proc/lampmap-e-19-6.proc
+./wmcu_configurator.py --host 192.168.104.18 --set-assigned-lamps ../lampmaps/proc/lampmap-e-18-7.proc
+./wmcu_configurator.py --host 192.168.104.17 --set-assigned-lamps ../lampmaps/proc/lampmap-e-17-8.proc
+./wmcu_configurator.py --host 192.168.104.16 --set-assigned-lamps ../lampmaps/proc/lampmap-e-16-9.proc
+./wmcu_configurator.py --host 192.168.104.15 --set-assigned-lamps ../lampmaps/proc/lampmap-e-15-10.proc
+./wmcu_configurator.py --host 192.168.104.14 --set-assigned-lamps ../lampmaps/proc/lampmap-e-14-11.proc
+
+./wmcu_configurator.py --host 192.168.103.12 --set-assigned-lamps ../lampmaps/proc/lampmap-e-12-0.proc
+./wmcu_configurator.py --host 192.168.103.11 --set-assigned-lamps ../lampmaps/proc/lampmap-e-11-1.proc
+./wmcu_configurator.py --host 192.168.103.10 --set-assigned-lamps ../lampmaps/proc/lampmap-e-10-2.proc
+./wmcu_configurator.py --host 192.168.103.9 --set-assigned-lamps ../lampmaps/proc/lampmap-e-09-3.proc
+./wmcu_configurator.py --host 192.168.103.8 --set-assigned-lamps ../lampmaps/proc/lampmap-e-08-4.proc
+./wmcu_configurator.py --host 192.168.103.7 --set-assigned-lamps ../lampmaps/proc/lampmap-e-07-5.proc
+./wmcu_configurator.py --host 192.168.103.6 --set-assigned-lamps ../lampmaps/proc/lampmap-e-06-6.proc
+./wmcu_configurator.py --host 192.168.103.5 --set-assigned-lamps ../lampmaps/proc/lampmap-e-05-7.proc
+./wmcu_configurator.py --host 192.168.103.4 --set-assigned-lamps ../lampmaps/proc/lampmap-e-04-8.proc
+
+./wmcu_configurator.py --host 192.168.102.19 --set-assigned-lamps ../lampmaps/proc/lampmap-w-19-0.proc
+./wmcu_configurator.py --host 192.168.102.18 --set-assigned-lamps ../lampmaps/proc/lampmap-w-18-1.proc
+./wmcu_configurator.py --host 192.168.102.17 --set-assigned-lamps ../lampmaps/proc/lampmap-w-17-2.proc
+./wmcu_configurator.py --host 192.168.102.16 --set-assigned-lamps ../lampmaps/proc/lampmap-w-16-3.proc
+./wmcu_configurator.py --host 192.168.102.15 --set-assigned-lamps ../lampmaps/proc/lampmap-w-15-4.proc
+./wmcu_configurator.py --host 192.168.102.14 --set-assigned-lamps ../lampmaps/proc/lampmap-w-14-5.proc
+./wmcu_configurator.py --host 192.168.102.13 --set-assigned-lamps ../lampmaps/proc/lampmap-w-13-6.proc
+./wmcu_configurator.py --host 192.168.102.12 --set-assigned-lamps ../lampmaps/proc/lampmap-w-12-7.proc
+
+./wmcu_configurator.py --host 192.168.101.10 --set-assigned-lamps ../lampmaps/proc/lampmap-w-10-0.proc
+./wmcu_configurator.py --host 192.168.101.9 --set-assigned-lamps ../lampmaps/proc/lampmap-w-09-1.proc
+./wmcu_configurator.py --host 192.168.101.8 --set-assigned-lamps ../lampmaps/proc/lampmap-w-08-2.proc
+./wmcu_configurator.py --host 192.168.101.7 --set-assigned-lamps ../lampmaps/proc/lampmap-w-07-3.proc
+./wmcu_configurator.py --host 192.168.101.6 --set-assigned-lamps ../lampmaps/proc/lampmap-w-06-4.proc
+./wmcu_configurator.py --host 192.168.101.5 --set-assigned-lamps ../lampmaps/proc/lampmap-w-05-5.proc
+./wmcu_configurator.py --host 192.168.101.4 --set-assigned-lamps ../lampmaps/proc/lampmap-w-04-6.proc
+
