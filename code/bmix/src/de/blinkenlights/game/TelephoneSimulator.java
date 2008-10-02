fe214b3474cdfef1466515eff5fc2b7b9fe7b7cd
andrew <andrew@fe0de55c-ccef-0310-be5c-e8635c8c2365>
2008-10-02 01:07:29 +0000

    Organize imports
    git-svn-id: file:///home/daniel/src/blinkenlights/legacy/svn/blinkenlights/trunk@737 fe0de55c-ccef-0310-be5c-e8635c8c2365

--- code/bmix/src/de/blinkenlights/game/TelephoneSimulator.java
+++ code/bmix/src/de/blinkenlights/game/TelephoneSimulator.java
@@ -7,15 +7,12 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.concurrent.Semaphore;
 
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class TelephoneSimulator implements UserInputSource {
 
