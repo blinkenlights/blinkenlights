9fea64621dd6b6d60d76e5a121b57f8df21ae8d0
dfraser <dfraser@fe0de55c-ccef-0310-be5c-e8635c8c2365>
2008-10-02 04:04:39 +0000

    Implemented the inverse direction for all command types (so we can make a bvoip client in java)
    git-svn-id: file:///home/daniel/src/blinkenlights/legacy/svn/blinkenlights/trunk@744 fe0de55c-ccef-0310-be5c-e8635c8c2365

--- code/bvoip/src/de/blinkenlights/bvoip/blt/BLTCommand.java
+++ code/bvoip/src/de/blinkenlights/bvoip/blt/BLTCommand.java
@@ -57,7 +57,7 @@ public class BLTCommand {
 		command = parts[1];
 		BLTCommand retval;
 		if ("register".equals(command)) {
-			logger.fine("register command");
+			logger.fine("got register command");
 			retval = new BLTCommand(channel, CommandType.REGISTER);
 			if (parts.length > 2) {
 				// optional destination port
@@ -65,15 +65,15 @@ public class BLTCommand {
 			}
 		}
 		else if ("heartbeat".equals(command)) {
-			logger.fine("heartbeat command");
+			logger.fine("got heartbeat command");
 			retval = new BLTCommand(channel, CommandType.HEARTBEAT);
 		}
 		else if ("accept".equals(command)) {
-			logger.fine("accept command");
+			logger.fine("got accept command");
 			retval = new BLTCommand(channel, CommandType.ACCEPT);
 		}
 		else if ("hangup".equals(command)) {
-			logger.fine("hangup command");
+			logger.fine("got hangup command");
 			retval = new BLTCommand(channel, CommandType.HANGUP);
 			if (parts.length > 2) {
 				// optional reason
@@ -82,28 +82,62 @@ public class BLTCommand {
 
 		}
 		else if ("playbackground".equals(command)) {
-			logger.fine("playbackground command");
+			logger.fine("got playbackground command");
 			retval = new BLTCommand(channel, CommandType.PLAYBACKGROUND);
 			if (parts.length > 2) {
 				// filename to play
 				retval.args.add(parts[2]);
 			} else {
-				throw new IllegalArgumentException("playbackground requires one argument, got 0.");
+				throw new IllegalArgumentException("playbackground requires one argument, got " + (parts.length - 2));
 			}
 		}
 		else if ("play".equals(command)) {
-			logger.fine("play command");		
+			logger.fine("got play command");		
 			retval = new BLTCommand(channel, CommandType.PLAY);
 			if (parts.length > 2) {
 				// filename to play
 				retval.args.add(parts[2]);
 			} else {
-				throw new IllegalArgumentException("play requires one argument, got 0.");
+				throw new IllegalArgumentException("play requires one argument, got " + (parts.length - 2));
+			}
+		}
+		else if ("onhook".equals(command)) {
+			logger.fine("got onhook command");		
+			retval = new BLTCommand(channel, CommandType.ONHOOK);
+		}
+		else if ("setup".equals(command)) {
+			retval = new BLTCommand(channel, CommandType.SETUP);
+			if (parts.length > 3) {
+				retval.args.add(parts[2]);
+				retval.args.add(parts[3]);
+			} else {
+				throw new IllegalArgumentException("setup requires two arguments, got " + (parts.length - 2));
+			}
+		}
+		else if ("setup".equals(command)) {
+			logger.fine("got setup command");		
+			retval = new BLTCommand(channel, CommandType.SETUP);
+		}
+		else if ("dtmf".equals(command)) {
+			retval = new BLTCommand(channel, CommandType.DTMF);
+			if (parts.length > 2) {
+				retval.args.add(parts[2]);
+			} else {
+				throw new IllegalArgumentException("dtmf requires two arguments, got " + (parts.length - 2));
+			}
+		}
+		else if ("error".equals(command)) {
+			retval = new BLTCommand(channel, CommandType.ERROR);
+			if (parts.length > 2) {
+				retval.args.add(parts[2]);
+			} else {
+				throw new IllegalArgumentException("error requires two arguments, got " + (parts.length - 2));
 			}
 		}
 		else {
 			throw new IllegalArgumentException("unknown command received: " + command);
 		}
+		
 		logger.finest("returning "+retval.command+" Command with arguments: "+retval.args);
 		return retval;
 	}
@@ -147,6 +181,46 @@ public class BLTCommand {
 			}
 			output = channelNum+":"+"error:"+args.get(0);
 		}
-		return output.getBytes();
+		else if (command == CommandType.REGISTER) {
+			if (args.size() != 0) {
+				throw new IllegalArgumentException("error requires 0 argument, please don't use the dstport option! you provided "+args.size());
+			}
+			output = "0:register";
+		}
+		else if (command == CommandType.HEARTBEAT) {
+			if (args.size() != 0) {
+				throw new IllegalArgumentException("error requires 0 argument, you provided "+args.size());
+			}
+			output = "0:heartbeat";
+		}
+		else if (command == CommandType.ACCEPT) {
+			if (args.size() != 0) {
+				throw new IllegalArgumentException("error requires 0 argument, you provided "+args.size());
+			}
+			output = channelNum+":"+"accept";
+		}
+		else if (command == CommandType.HANGUP) {
+			if (args.size() > 1) {
+				throw new IllegalArgumentException("error requires 0 or 1 arguments, you provided "+args.size());
+			}
+			String reason = "";
+			if (args.size() == 1) {
+				reason = ":"+args.get(0);
+			}
+			output = channelNum+":"+"hangup"+reason;
+		}
+		else if (command == CommandType.PLAYBACKGROUND) {
+			if (args.size() != 1) {
+				throw new IllegalArgumentException("error requires 1 arguments, you provided "+args.size());
+			}
+			output = channelNum+":"+"playbackground:"+args.get(0);
+		}
+		else if (command == CommandType.PLAY) {
+			if (args.size() != 1) {
+				throw new IllegalArgumentException("error requires 1 argument, you provided "+args.size());
+			}
+			output = channelNum+":"+"play:"+args.get(0);
+		}
+		return output.getBytes();	
 	}
 }
