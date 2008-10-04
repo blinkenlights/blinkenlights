86ceb7ad1ec8d83f55452159bda54cc3712b7952
dom <dom@fe0de55c-ccef-0310-be5c-e8635c8c2365>
2008-10-04 00:55:36 +0000

    - added cascade information collecting to blinkenproxy
    git-svn-id: file:///home/daniel/src/blinkenlights/legacy/svn/blinkenlights/trunk@800 fe0de55c-ccef-0310-be5c-e8635c8c2365

--- code/utilities/ruby/blinkenproxy.rb
+++ code/utilities/ruby/blinkenproxy.rb
@@ -11,13 +11,15 @@
 #   blinkenproxy.rb [-v | -h] [-f] [-a] [blinkenproxyaddress:port | receive-port] [heartbeat-port]
 #
 #   For help use: blinkenproxy.rb -h
-#   defaults are localhost:2323 and heartbeat-port:4242
+#   defaults are 2323 and heartbeat-port:4242
 # == Options
 #   -h, --help          Displays help message
 #   -v, --version       Display the version, then exit
 #   -f, --frames        Output frame content
 #   -t, --timestamp     Output 64-bit timestamp instead of human redable time
 #   -a, --asciidisplay  Output frames as ascii using ' .,:;+io!4768%@$'
+#   -r, --restrictip ADDRESSES  restrict receiving to ip addresses, comma separated
+
 # == Author
 #   Dominik Wagner
 # == Copyright
@@ -41,7 +43,7 @@ class App
   def initialize(arguments, stdin)
     @arguments = arguments
     @stdin = stdin
-    
+    @last_frame_time = Time.now
     @clients = {}
     
     # Set defaults
@@ -50,6 +52,8 @@ class App
     @options.heartbeatPort = 4242
     @options.showFrames = false
     @semaphore = Mutex.new
+    @options.sendClients = true
+    @options.restrictips = []
   end
 
   # Parse options, check arguments, then process the command
@@ -81,6 +85,8 @@ class App
       opts.on('-f', '--frames')   { @options.showFrames = true }
       opts.on('-t', '--timestamp')  { @options.timestamp = true }
       opts.on('-a', '--asciidisplay')  { @options.ascii = true }
+      opts.on('-d', '--dontsendstats') {@options.sendClients = false }
+      opts.on('-r', '--restrictip IP_ADDRESSES') {|addresses| @options.restrictips = addresses.split(',')}
       # TO DO - add additional options
             
       opts.parse!(@arguments) rescue return false
@@ -140,29 +146,56 @@ class App
       puts "#{File.basename(__FILE__)} version #{VERSION}"
     end
     
+    def indirect_count
+      @clients.inject(0){|m,pair| m = m + pair[1][:direct_client_count] + pair[1][:indirect_client_count]}
+    end
+    
+    
     def send_heartbeat
-      data = [0x42424242,0,0,0].pack("NnnN")
+      data = [0x42424242,0,0,0].pack("NnnN") # old blinkenproxy compatibility , needs long packet
+      if @options.sendClients
+        @semaphore.synchronize {
+          data = [0x42424242,0, @clients.length, self.indirect_count ].pack("NnnN")
+        }
+      end
       @socket.send(data,0,@options.proxyAddress,@options.proxyPort)
-      print Time.now.strftime("%Y-%m-%d %H:%M:%S") + ": -------> Sent ping to #{@options.proxyAddress} #{@options.proxyPort} \n"
+      seconds_since_last_frame = Time.now - @last_frame_time
+      print Time.now.strftime("%Y-%m-%d %H:%M:%S") + ": -------> Sent ping to #{@options.proxyAddress} #{@options.proxyPort} " + " - last frame %0.2f seconds ago\n" % [seconds_since_last_frame] if seconds_since_last_frame > 30.0
       sleep(5)
+      rescue 
+        p $!
     end
 
     def receive_heartbeats
       data,client = @proxySocket.recvfrom(1000)
+      change_string = "="
+      should_report = false
       if data.unpack("N")[0] == 0x42424242
         clientaddress = "%s:%d" % [client[3],client[1]]
+        direct_client_count,indirect_client_count = 0,0
+        magic,version,direct_client_count,indirect_client_count = data.unpack("NnnN") if (data.length >= 4+4+4)
         @semaphore.synchronize {
           clienthash = @clients[clientaddress]
           if clienthash.nil?
-            clienthash = {:updateTime => Time.now, :client => client}
+            clienthash = {:updateTime => Time.now, :client => client, :direct_client_count => direct_client_count, :indirect_client_count => indirect_client_count}
             @clients[clientaddress] = clienthash
-            print Time.now.strftime("%Y-%m-%d %H:%M:%S"), ": (+) #{clientaddress} (#{@clients.length})\n"
-            STDOUT.flush
+            change_string = "+"
+            should_report = true
+          else
+            should_report = (direct_client_count != clienthash[:direct_client_count] || clienthash[:indirect_client_count] != indirect_client_count)
+            clienthash[:direct_client_count]    = direct_client_count
+            clienthash[:indirect_client_count]  = indirect_client_count
+            clienthash[:updateTime] = Time.now
           end
           
-          clienthash[:updateTime] = Time.now
+          if should_report
+            print Time.now.strftime("%Y-%m-%d %H:%M:%S"), ": (#{change_string} #{clientaddress} #{direct_client_count},#{indirect_client_count}) total:[#{@clients.length},#{self.indirect_count}]\n"
+            STDOUT.flush
+          end
         }
       end
+      rescue 
+        p $!
     end #def
 
     
@@ -211,11 +244,12 @@ class App
 
         now = Time.now
         if magic == 0x23542666 || magic == 0x23542668
+          @last_frame_time = Time.now
           #send it on
           @semaphore.synchronize {
             if (now - lastCheck > 5.0) 
               unless @clients.reject!{ |k,v| now - v[:updateTime] > 5.0 * 12}.nil?
-                print Time.now.strftime("%Y-%m-%d %H:%M:%S"), ": (-) (#{@clients.length})\n"
+                print Time.now.strftime("%Y-%m-%d %H:%M:%S"), ": (-) total:[#{@clients.length},#{self.indirect_count}]\n"
                 STDOUT.flush
               end
             end
